5a3f9c5b625d06c183add367a46d0107c11b94a0
allexbelllo <allexbelllo@gmail.com>
2026-04-20 12:08:02 +0100

    Remove LoginManager from extensions.py
    Removed LoginManager initialization and its login view setting.

--- extensions.py
+++ extensions.py
@@ -1,8 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-from flask_login import LoginManager
 
 db = SQLAlchemy()
 mail = Mail()
-login_manager = LoginManager()
-login_manager.login_view = "login"
+
